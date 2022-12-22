4f65fc44ad0322a05b5e87970b4c2dcb6f47079e
doantruc <doantruc1989@gmail.com>
2022-12-22 14:57:24 +0700

    second

--- src/other/axios.js
+++ src/other/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const token = localStorage.hasOwnProperty('user') ? (JSON.parse(localStorage.getItem("user"))).access_token : '';
 const instance = axios.create({
-    baseURL: 'https://d3f0-115-79-210-160.ap.ngrok.io/',
+    baseURL: 'http://605f-115-79-210-160.ngrok.io/',
     timeout: 1000,
     headers: { 'Authorization': 'Bearer ' + token },
 
--- src/other/axiosAll.js
+++ src/other/axiosAll.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const axiosAll = axios.create({
-    baseURL: 'https://d3f0-115-79-210-160.ap.ngrok.io/',
+    baseURL: 'http://605f-115-79-210-160.ngrok.io/',
     timeout: 1000,
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+        'Content-Type': 'application/json',
+    },
     withCredentials: true
 });
 
