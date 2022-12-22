05778bdc9ccfd89b5561af85784453f87013ccde
doantruc <doantruc1989@gmail.com>
2022-12-22 16:01:10 +0700

    seventh

--- src/other/axios.js
+++ src/other/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = localStorage.hasOwnProperty('user') ? (JSON.parse(localStorage.getItem("user"))).access_token : '';
 const instance = axios.create({
     baseURL: 'https://605f-115-79-210-160.ngrok.io',
-    headers: { 'Authorization': 'Bearer ' + token },
+    headers: { Authorization: `Bearer ${token}` },
 });
 
 export default instance;
